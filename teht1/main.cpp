62d976d0739037b1965288edda89866e8684b11d
Nella <nellasoudunsaari@gmail.com>
2025-01-09 14:17:09 +0200

    ekan tehtävän alku, jää tulostamaan arvaa lukua

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int satunnainenluku = 20;
+    int arvaus;
+    while(true){
+        cout << "Arvaa luku: " << endl;
+        cin >> arvaus;
+        cout << "Arvauksesi: " << arvaus << endl;
+        if(arvaus == satunnainenluku){
+            cout << "Arvasit oikein!"
+                 << endl;
+            break;
+        } else if (arvaus > satunnainenluku){
+            cout << "Luku on liian suuri"
+                 << endl;
+        } else if ( arvaus < satunnainenluku){
+            cout << "Luku on liian pieni"
+                 << endl;
+        } else {
+            cout << "Virhe?"
+                 << endl;
+        }
+    }
+
     return 0;
 }
